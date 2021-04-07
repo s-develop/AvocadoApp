0e8430d11ac3381ef318c51ee301e08f7f52ed57
Sergei Matveev <sgsq@ya.ru>
2021-04-07 19:48:28 +0400

    Initial Commit

---
+++ Avocados/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Avocados
+//
+//  Created by Sergei on 07.04.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
